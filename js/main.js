83a53e4cfe1181042161bd836f9d9de8b84a283d
Svetlana Sedova <svt.sedova@gmail.com>
2023-07-12 15:35:09 +0300

    Добавляет валидацию формы и полей для коммента и хеш-тегов, удаляет подписки

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
 import { renderGallery } from './gallery.js';
+import { initializeUploadForm } from './img-upload-form.js';
+import { createPristineInstance, correctInputData } from './validation.js';
 
 renderGallery();
+
+const { imgUploadForm, closeImgOverlayForm } = initializeUploadForm();
+const pristine = createPristineInstance();
+
+imgUploadForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  if (pristine.validate()) {
+    // Корректирует введенные данные перед отправкой
+    correctInputData();
+    // После успешной валидации отправляем форму на сервер
+    imgUploadForm.submit();
+    // Сбрасываем все поля формы и закрываем форму
+    imgUploadForm.reset();
+    closeImgOverlayForm();
+  }
+
+});
+
